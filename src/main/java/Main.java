cc3d6af22ee0e041138c8da617cb720b66cacef9
Elena <36508011+Ferst1@users.noreply.github.com>
2023-09-26 15:16:42 +0200

    task1

---
+++ src/main/java/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char charVar = 'G';
+        int intVar = 89;
+        byte byteVar = 4;
+        short shortVar = 56;
+        float floatVar = 4.7333436f;
+        double doubleVar = 4.355453532;
+        long longVar = 12121;
+
+        // Вывод в консоль
+//        System.out.println("char: " + charVar);
+//        System.out.println("int: " + intVar);
+//        System.out.println("byte: " + byteVar);
+//        System.out.println("short: " + shortVar);
+//        System.out.println("float: " + floatVar);
+//        System.out.println("double: " + doubleVar);
+//        System.out.println("long: " + longVar);
+
+
+//        ----------------------------
+
+
+        System.out.println("Character: " + charVar);
+        System.out.println("Integer: " + intVar);
+        System.out.println("Byte: " + byteVar);
+        System.out.println("Short: " + shortVar);
+        System.out.println("Float: " + floatVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Long: " + longVar);
+    }
+}
+
+
+//1. В методе main инициализировать все примитивные типы и не примитивные типы.
+//        2. Вывести их результат в консоль:
+//        char: G
+//        int: 89
+//        byte: 4
+//        short: 56
+//        float: 4.7333436
+//        double: 4.355453532
+//        long: 12121
+//
+//        Character: G
+//        Integer: 89
+//        Byte: 4
+//        Short: 56
+//        Float: 4.7333436
+//        Double: 4.355453532
+//        Long: 12121
